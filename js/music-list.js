9c975d0e5593f428c7176d94eede5ec1d31fa01e
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2023-01-31 14:25:47 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,52 @@
+
+let allMusic = [
+  {
+    name: "The Real Slim Shady",
+    artist: "Eminem",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Racks n Rounds",
+    artist: "Sidhu Moosewala",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "94 Flow",
+    artist: "Big Boi Deep",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Mafia Style",
+    artist: "Sidhu Moosewala",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Peaches",
+    artist: "Diljit Dosanjh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "WYTB",
+    artist: "Karan Aujla",
+    img: "music-8",
+    src: "music-8"
+  },
+
+];
